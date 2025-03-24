0b9c54a1cd77a1b58887c4804928444f0eaf0e7f
Luis Baquedano <lfbcordova@gmail.com>
2025-03-24 07:47:34 -0500

    Copiar archivo main.js de feature branch

---
+++ main.js
@@ -0,0 +1,18 @@
+console.log('Hola mundo')
+
+function isNumPar(n) {
+    return (n % 2) === 0;
+}
+
+function sumar(a, b) {
+    return Number.parseFloat(a) + Number.parseFloat(b);
+}
+
+
+function restar(a, b) {
+    return Number.parseFloat(a) - Number.parseFloat(b);
+}
+
+function multplicar(a, b) {
+    return Number.parseFloat(a) * Number.parseFloat(b);
+}
